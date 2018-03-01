64b90f433fe93feb5bfeff2aee22da6ab10b1a5b
artyeug <artyeug@gmail.com>
2018-03-01 12:12:49 +0700

    Add an implementation for second lab-work

---
+++ repetition/src/StringsTorture.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class ExecStringsTorture implements IProgram {
+    private final String programName = "Strings Torture (lab-work-02)";
+    public String getProgramName() { return programName; }
+
+    public void execute() throws Exception {
+        System.out.println("Write your string:");
+
+        Scanner scanner = new Scanner(System.in);
+        String inputStr = scanner.nextLine();
+
+        StringsTorture tort = new StringsTorture(inputStr);
+        String result = tort.performAction();
+        System.out.printf("Result:\n%s", result);
+    }
+}
+
+public class StringsTorture {
+
+    private String initialString;
+
+    StringsTorture(String string) throws Exception {
+        if (string.isEmpty())
+            throw new Exception("StringsTorture constructor: argument (string:String) was empty.");
+
+        initialString = string;
+    }
+
+    public String performAction() {
+        String formattedString = initialString;
+        formattedString = formattedString.replaceAll(" ", ", ");
+
+        return formattedString;
+    }
+}
