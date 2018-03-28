4c022dcaf8ebcd316f80ceeafeae67632ddd66e3
artyeug <artyeug@gmail.com>
2018-03-28 11:44:13 +0700

    Minor comment lines refactoring

--- repetition/src/IProgram.java
+++ repetition/src/IProgram.java
@@ -1,6 +1,6 @@
-/// This interface (IProgram) is used for creating better console interaction with
-/// separated laboratory works. One can run this project and select any program for
-/// the specified laboratory work.
+// This interface (IProgram) is used for creating better console interaction with
+// separated laboratory works. One can run this project and select any program for
+// the specified laboratory work.
 
 public interface IProgram {
     void execute() throws Exception;
