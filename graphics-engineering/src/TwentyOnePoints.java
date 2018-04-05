14463fe60140e819597f2f98ddc4afcbe901372d
artyeug <artyeug@gmail.com>
2018-04-06 00:40:03 +0700

    Add a non-completed implementation of the game

--- graphics-engineering/src/TwentyOnePoints.java
+++ graphics-engineering/src/TwentyOnePoints.java
@@ -1,12 +1,15 @@
 import java.util.Collections;
+import java.util.Stack;
 import java.util.Vector;
 
 public class TwentyOnePoints {
     public static final int DECK_SIZE = 36;
-    private Vector<Card> _cardDeck;
+    private Stack<Card> _cardDeck;
+    private Vector<CardPlayer> _cardPlayers;
 
     TwentyOnePoints() throws Exception {
         _cardDeck = CardDeckCreator.createDeck(DECK_SIZE);
+        _cardPlayers = new Vector<>();
     }
 
     public void displayDeck() {
@@ -20,11 +23,19 @@ public class TwentyOnePoints {
         System.out.println("Success.");
     }
 
+    // ..
+
     public void playGame() {
         System.out.println("Hello and welcome to 'Twenty One Points' game!");
         System.out.println("The deck will right now be shuffled.");
         shuffleDeck();
-        System.out.println("You will be playing against computer. He (it) will be a vendor (for now).");
-        // ...
+        System.out.println("You will be playing against computer. He (it) will be a dealer (for now).");
+
+        _cardPlayers.add(new Computer());
+        _cardPlayers.add(new Player());
+        _cardPlayers.elementAt(0).setDealer(true);
+
+
+//        if (computer.getClass() == Computer.class)
     }
 }
