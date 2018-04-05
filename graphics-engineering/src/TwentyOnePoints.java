8967bc156b1f779ba98167fc52a666a10b658324
artyeug <artyeug@gmail.com>
2018-04-05 13:48:35 +0700

    Add an interaction (not completed yet)

--- graphics-engineering/src/TwentyOnePoints.java
+++ graphics-engineering/src/TwentyOnePoints.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.Vector;
 
 public class TwentyOnePoints {
@@ -11,5 +12,19 @@ public class TwentyOnePoints {
     public void displayDeck() {
         for (Card card : _cardDeck)
             card.show();
+    }
+
+    private void shuffleDeck() {
+        System.out.print("Shuffling the deck.. ");
+        Collections.shuffle(_cardDeck);
+        System.out.println("Success.");
+    }
+
+    public void playGame() {
+        System.out.println("Hello and welcome to 'Twenty One Points' game!");
+        System.out.println("The deck will right now be shuffled.");
+        shuffleDeck();
+        System.out.println("You will be playing against computer. He (it) will be a vendor (for now).");
+        // ...
     }
 }
