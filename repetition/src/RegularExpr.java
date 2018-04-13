de1a013f089c4505876268261df3f74c3ca14b68
artyeug <artyeug@gmail.com>
2018-04-13 13:04:11 +0700

    Remove unnecessary exception's throwing

--- repetition/src/RegularExpr.java
+++ repetition/src/RegularExpr.java
@@ -5,7 +5,7 @@ class ExecRegularExpr implements IProgram {
     private final String programName = "Regular Expression (lab-work-04(02))";
     public String getProgramName() { return programName; }
 
-    public void execute() throws Exception {
+    public void execute() {
         System.out.print("Write a hexadecimal value of color (example: #FF0000, #bf4):\n> ");
 
         Scanner scanner = new Scanner(System.in);
@@ -23,9 +23,9 @@ final class RegularExpr {
     private static final String _hexColorPattern = "#[a-fA-F_0-9]{6}"; // hash and any of (a-f) letter and (0-9) digit 6 times
     private static final String _hexColorPatternShort = "#[a-fA-F_0-9]{3}"; // the same as above but only 3 times (short hex color notation)
 
-    public static boolean matchesHexColor(String initialString) throws Exception {
+    public static boolean matchesHexColor(String initialString) {
         if (initialString == null || initialString.isEmpty())
-            throw new Exception("matchesHexColor: initial string is null or empty.");
+            throw new IllegalArgumentException("matchesHexColor: initial string is null or empty.");
 
         return Pattern.matches(_hexColorPattern, initialString)
                 || Pattern.matches(_hexColorPatternShort, initialString);
