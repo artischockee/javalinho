b369c2a0196ed53a500665c54bf645bbc859e69e
artyeug <artyeug@gmail.com>
2018-04-13 13:18:04 +0700

    Rename MeinKollektion to MyCollection

--- repetition/src/Collection.java
+++ repetition/src/Collection.java
@@ -9,7 +9,7 @@ public class Collection {
     Collection() {
         programs = new Vector<>(Arrays.asList(
                 new ExecUselessActions(), new ExecStringsTorture(), new ExecRussianText(),
-                new ExecRegularExpr(), new ExecMeinKollektion()));
+                new ExecRegularExpr(), new ExecMyCollection()));
     }
 
     public void display() {
