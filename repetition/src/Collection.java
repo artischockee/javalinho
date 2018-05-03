b7f37630177e4ae8d11e4e242d6dd206a15d1610
artyeug <artyeug@gmail.com>
2018-05-03 12:20:50 +0700

    Make the class pseudo-static

--- repetition/src/Collection.java
+++ repetition/src/Collection.java
@@ -1,28 +1,40 @@
 import java.util.Arrays;
 import java.util.Vector;
 
-// Class 'Collection' is used for storing the programs' executors.
-public class Collection {
-    private final int initialIndexOfProgram = 1;
-    private final Vector<Program> programs;
+// This class is used for storing the programs' executors
 
-    Collection() {
-        programs = new Vector<>(Arrays.asList(
-                new ExecUselessActions(), new ExecStringsTorture(), new ExecRussianText(),
-                new ExecRegularExpr(), new ExecMyCollection()));
+final class Collection {
+    private static final int INIT_PROGRAM_INDEX = 1;
+    private static final Vector<Program> PROGRAMS;
+
+    static {
+        PROGRAMS = new Vector<>(Arrays.asList(
+            new ExecUselessActions(),
+            new ExecStringsTorture(),
+            new ExecRussianText(),
+            new ExecRegularExpr(),
+            new ExecMyCollection(),
+            new ExecBitOperations()
+        ));
     }
 
-    public void display() {
-        int index = initialIndexOfProgram;
-        for (Program program : programs) {
+    static final int LOWER_BOUND = INIT_PROGRAM_INDEX; // just for better semantics
+    static final int UPPER_BOUND = PROGRAMS.size();
+
+    // 'private' used to escape an initializing of this pseudo-static class
+    private Collection() {}
+
+    static void display() {
+        int index = INIT_PROGRAM_INDEX;
+
+        for (Program program : PROGRAMS)
             System.out.printf("[%d] %s\n", index++, program.getProgramName());
-        }
     }
 
-    public void execute(int programIndex) throws Exception {
-        if (programIndex < initialIndexOfProgram || programIndex > programs.size())
+    static void execute(int programIndex) throws Exception {
+        if (programIndex < INIT_PROGRAM_INDEX || programIndex > PROGRAMS.size())
             throw new Exception();
 
-        programs.get(--programIndex).execute();
+        PROGRAMS.elementAt(--programIndex).execute();
     }
 }
